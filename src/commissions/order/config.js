3314d42336e9787ea9383d07ac9adec234aac26a
AnushaMotamarri <motamarrianusha01@gmail.com>
2025-05-06 23:26:40 +0530

    committed config file

---
+++ src/commissions/order/config.js
@@ -0,0 +1,29 @@
+const content ={
+    description:'We, as humans, are deeply emotional beings. Life brings out the best and the worst in us—and photographs are one of the most powerful ways to preserve those feelings. They connect us to our past, remind us of people, places, and moments, and help us understand who we are and what our loved ones mean to us.',
+    weDeliver:'We specialize in creating all types of portraits—from traditional pencil sketches to digital artwork.Whether it’s for a birthday, wedding, housewarming, or any special event, we’ve got you covered.We also design personalized artwork and illustrations for your social media platforms.',
+    steps :[{
+        title:'Booking',
+        description:'You can book your slot via social media, WhatsApp, or email.',
+    },{
+        title:'Advance Payment',
+        description:'A 50% advance is required at the time of booking to confirm your slot.'
+    },{
+        title: 'Drafts & Feedback',
+        description:'Within 2 days of booking, you will receive 3 rough drafts, You can request changes to these drafts.',
+        children:['Sketch only','Colored version','Background added']
+    },{
+        title:'Final Delivery',
+        children:['Final delivery takes 4 to 10 days, depending on the category chosen.','Upon full payment, you’ll receive a high-resolution soft copy of the final portrait.','If you want a framed print shipped, additional charges will apply (based on size and location).']
+    },{
+        title:'Video Usage',
+        description:'We often share time-lapse videos of the portrait creation process on our social media to showcase our work. If you\'d prefer your commission not be shared, please let us know explicitly at the time of booking.',
+        
+    },{
+        title:'Extra Charges',
+        description:'If the portrait includes more than 2 people, an additional ₹1,000 per person will be charged.'
+    }
+
+]
+} 
+
+export {content}
